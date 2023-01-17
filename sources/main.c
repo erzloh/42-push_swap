5e84158f4fb537dcd07ce26e6384401700976dcf
Eric Holzer <eric.holzer.lsn@gmail.com>
2023-01-17 14:34:07 +0100

    Add all instruction functions :)

--- sources/main.c
+++ sources/main.c
@@ -3,59 +3,34 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: eric <eric@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: eholzer <eholzer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/10 21:28:54 by eric              #+#    #+#             */
-/*   Updated: 2023/01/14 18:07:46 by eric             ###   ########.fr       */
+/*   Created: 2023/01/16 09:31:08 by eholzer           #+#    #+#             */
+/*   Updated: 2023/01/17 14:34:07 by eholzer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	*get_arg_tab(int ac, char **av)
-{
-	int	*stack_a;
-	int	i;
-
-	stack_a = malloc(sizeof(int) * ac - 1);
-	i = 0;
-	while (i < ac - 1)
-	{
-		stack_a[i] = ft_atoi(av[i + 1]);
-		i++;
-	}
-	return (stack_a);
-}
-
-void	print_args(int *tab, int size)
-{
-	int	i;
-
-	i = 0;
-	while (i < size)
-	{
-		ft_printf("%d\n", tab[i]);
-		i++;
-	}
-}
-
 int	main(int ac, char **av)
 {
-	t_list	*node_a;
-	int		i;
-	int	*arg_array;
+	t_stack	stack_a;
+	t_stack	stack_b;
 
-	arg_array = get_arg_tab(ac, av);
-	node_a = ft_lstnew(&arg_array[0]);
-	print_args(arg_array, ac - 1);
-	i = 1;
-	while (i < ac - 1)
-	{
-		node_a->next = ft_lstnew(&arg_array[i]);
-		node_a = node_a->next;
-		i++;
-	}
-	node_a->next = NULL;
-
+	init_stack_a(ac, av, &stack_a);
+	stack_b.tab = NULL;
+	stack_b.size = 0;
+	// sa(&stack_a);
+	// pb(&stack_a, &stack_b);
+	ra(&stack_a);
+	pb(&stack_a, &stack_b);
+	pb(&stack_a, &stack_b);
+	rb(&stack_b);
+	ft_printf("STACK A ----------\n");
+	print_tab(stack_a);
+	ft_printf("STACK B ----------\n");
+	print_tab(stack_b);
+	free(stack_a.tab);
+	free(stack_b.tab);
 	return (0);
 }
